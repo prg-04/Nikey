9813ea9558e85b3b3d85b664b1aa3eeb86c182ff
prg-04 <karanjaevans95@gmail.com>
2023-08-31 07:21:51 +0300

    Initialize nikey landing page

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+
+export {
+    SpecialOffers,
+    SuperQuality,
+    Services,
+    CustomerReviews,
+    Hero,
+    PopularProducts,
+    Subscribe,
+    Footer
+}
